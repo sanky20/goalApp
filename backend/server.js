3195795aa6a944e4f27e064a37e05768393058ae
Sanskar Sharma <sankysaa20@gmail.com>
2023-09-05 23:48:48 +0530

    Ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const colors = require('colors');
 const { errorHandler } = require('./middleware/errorMiddleware.js');
@@ -16,6 +17,18 @@ app.use(express.urlencoded({extended: false}))
 app.use('/api/goals' , require('./routes/goalRoutes.js'))
 app.use('/api/users' , require('./routes/userRoutes.js'))
 
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(
+        path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+      )
+    );
+  } else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+  }
 
 // @middleware for designing custom-made errorHandler
 app.use(errorHandler)
